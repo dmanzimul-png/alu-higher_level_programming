8c95428f2e062b1bbd86d02ff85e8f83ae2d55ae
Elvis <dmanzimul@alustudent.com>
2026-03-25 22:33:58 +0200

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square with size"""
+
+
+class Square:
+    """Square with private size"""
+
+    def __init__(self, size):
+        """Initialize square with size"""
+        self.__size = size
